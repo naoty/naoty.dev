0b92969087398d1bb13be64df7150724c45e0dba
Naoto Kaneko <naoty.k@gmail.com>
2020-02-24 00:39:23 +0900

    Create posts index pages for each tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
-exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions;
+const createPostPages = async ({ graphql, actions }) => {
   const result = await graphql(`
     {
       allMarkdownRemark {
@@ -19,18 +18,47 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     }
   `);
 
-  if (result.errors) {
-    reporter.panicOnBuild("Error while running GraphQL query");
-    return;
-  }
+  const { createPage } = actions;
+  const template = path.resolve("./src/templates/posts/_id.js");
 
-  const postTemplate = path.resolve("./src/templates/posts/_id.js");
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     const id = path.basename(node.fileAbsolutePath, ".md");
     createPage({
       path: `/posts/${id}.html`,
-      component: postTemplate,
+      component: template,
       context: node
     });
   });
+};
+
+const createTaggedPostsPages = async ({ graphql, actions }) => {
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        group(field: frontmatter___tags) {
+          tag: fieldValue
+        }
+      }
+    }
+  `);
+
+  const { createPage } = actions;
+  const template = path.resolve("./src/templates/posts/_tag/index.js");
+
+  result.data.allMarkdownRemark.group.forEach(({ tag }) => {
+    createPage({
+      path: `/posts/${tag}/`,
+      component: template,
+      context: { tag }
+    });
+  });
+};
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  try {
+    await createPostPages({ graphql, actions });
+    await createTaggedPostsPages({ graphql, actions });
+  } catch (e) {
+    reporter.panicOnBuild(`Error while running GraphQL query: ${e}`);
+  }
 };
---
+++ src/templates/posts/_tag/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Layout from "../../../components/layout";
+import { graphql, Link } from "gatsby";
+import styles from "./index.module.css";
+
+export default function TaggedPostsPage({
+  pageContext,
+  data: {
+    allMarkdownRemark: { edges }
+  }
+}) {
+  const links = edges.map(({ node }) => {
+    const basename = node.fileAbsolutePath.split("/").pop();
+    const id = basename.replace(".md", "");
+    return (
+      <li key={id}>
+        <Link to={`/posts/${id}.html`}>{node.frontmatter.title}</Link>
+      </li>
+    );
+  });
+
+  return (
+    <Layout>
+      <h1 className={styles.title}>#{pageContext.tag}</h1>
+      <ul>{links}</ul>
+      <nav>
+        <ul className={styles.navigation}>
+          <li className={styles.navigationItem}>
+            <Link to="/">Top</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/posts/">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___time] }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      edges {
+        node {
+          fileAbsolutePath
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
