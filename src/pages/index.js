cce98e8cea829c53ffd845c270345ee0cb3ccbe2
Naoto Kaneko <naoty.k@gmail.com>
2020-02-23 13:49:50 +0900

    Apply styles to root page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import React from "react";
+import styles from "./index.module.css";
 
 export default () => (
-  <>
-    <h1>Hello, gatsby.js!!!</h1>
-    <p>これはダミーテキストです。</p>
-  </>
+  <main className={styles.container}>
+    <h1 className={styles.title}>Naoto Kaneko</h1>
+    <ul>
+      <li>
+        <a href="#">Posts</a>
+      </li>
+      <li>
+        <a href="https://github.com/naoty">GitHub</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/naoty_k">Twitter</a>
+      </li>
+    </ul>
+  </main>
 );
