8ca95ff7b0ed39d420939a0748e14c23c1eba5ae
Naoto Kaneko <naoty.k@gmail.com>
2020-02-23 18:20:10 +0900

    Add links to each post pages

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,29 +1,36 @@
 import React from "react";
 import Layout from "../../components/layout";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 
-export default ({
+export default function PostsPage({
   data: {
     allMarkdownRemark: { edges }
   }
-}) => (
-  <Layout>
-    <h1>Posts</h1>
-    <ul>
-      {edges.map(edge => (
-        <li>
-          <a href="#">{edge.node.frontmatter.title}</a>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-);
+}) {
+  const links = edges.map(({ node }) => {
+    const basename = node.fileAbsolutePath.split("/").pop();
+    const id = basename.replace(".md", "");
+    return (
+      <li key={id}>
+        <Link to={`/posts/${id}.html`}>{node.frontmatter.title}</Link>
+      </li>
+    );
+  });
+
+  return (
+    <Layout>
+      <h1>Posts</h1>
+      <ul>{links}</ul>
+    </Layout>
+  );
+}
 
 export const query = graphql`
   query PostsQuery {
     allMarkdownRemark(sort: { order: DESC, fields: fileAbsolutePath }) {
       edges {
         node {
+          fileAbsolutePath
           frontmatter {
             title
           }
