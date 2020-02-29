3f6519633280c189f0b6beec3631a2506129d064
Naoto Kaneko <naoty.k@gmail.com>
2020-02-29 18:45:13 +0900

    Add logger to mermaid error handler
    Only production build encountered parse errors.

--- src/templates/posts/_id.js
+++ src/templates/posts/_id.js
@@ -12,6 +12,11 @@ export default class PostTemplate extends Component {
     if (elements.length === 0) return;
 
     mermaid.initialize({ startOnLoad: true });
+    mermaid.parseError = (error, hash) => {
+      console.error(error);
+      console.log(hash);
+    };
+
     this.componentDidUpdate();
   }
 
