440e451738bba831988fcb8adae2a9c07da31ca9
Naoto Kaneko <naoty.k@gmail.com>
2020-03-01 19:06:20 +0900

    Render diagrams on build time
    Rendering at client side sometimes fails.

--- plugins/gatsby-remark-mermaid/index.js
+++ plugins/gatsby-remark-mermaid/index.js
@@ -1,12 +1,56 @@
+const path = require("path");
+const puppeteer = require("puppeteer");
 const visit = require("unist-util-visit");
 
-module.exports = ({ markdownAST }, pluginOptions) => {
+const findMermaidNodes = markdownAST => {
+  const nodes = [];
+
   visit(markdownAST, "code", node => {
     if (node.lang !== "mermaid") return;
-
-    node.type = "html";
-    node.value = `<div class="mermaid">\n${node.value}\n</div>`;
+    nodes.push(node);
   });
+
+  return nodes;
+};
+
+const renderString = async (browser, text, key) => {
+  const page = await browser.newPage();
+  await page.goto(`file://${path.resolve(__dirname, "index.html")}`);
+  await page.addScriptTag({
+    path: require.resolve("mermaid/dist/mermaid.min.js")
+  });
+
+  const result = await page.evaluate(
+    (text, key) => {
+      return new Promise((resolve, reject) => {
+        try {
+          mermaid.initialize({ startOnLoad: true });
+          mermaid.mermaidAPI.render(`container-${key}`, text, svg =>
+            resolve(svg)
+          );
+        } catch (e) {
+          reject(e);
+        }
+      });
+    },
+    text,
+    key
+  );
+
+  return result;
+};
+
+module.exports = async ({ markdownAST }, pluginOptions) => {
+  const nodes = findMermaidNodes(markdownAST);
+  if (nodes.length === 0) return markdownAST;
+
+  const browser = await puppeteer.launch();
+  for (let node of nodes) {
+    const index = nodes.indexOf(node);
+    node.type = "html";
+    node.value = await renderString(browser, node.value, index);
+  }
+  await browser.close();
 
   return markdownAST;
 };
